b9618d28ecaf0aaaad6750b56a902b2d473a2b7a
Your Name <your.email@example.com>
2026-01-06 18:11:28 +0530

    oklof

--- app/src/app/about/page.tsx
+++ app/src/app/about/page.tsx
@@ -1,9 +1,13 @@
 import AboutHero from "@/components/pages/about/AboutHero";
+import WhatWeDo from "@/components/pages/about/WhatWeDo";
+import SuccessJourney from '@/components/pages/about/SuccessJourney';
 
 export default function AboutPage() {
     return (
         <main className="min-h-screen bg-white">
             <AboutHero />
+            <WhatWeDo />
+            <SuccessJourney />
         </main>
     );
 }
